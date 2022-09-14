7229fcda40c5faa3598bef6acf5ed592a8320a87
Vinit-Tiwari <95700013+Vinit-Tiwari@users.noreply.github.com>
2022-09-14 20:25:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ var input = document.getElementById("input");
 let count=0;
 
 input.addEventListener("keydown", function (e) {
-    if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+    if (e.key === "Enter") {  //checks whether the pressed key is "Enter"
         add();
     }
 });
 
-let l=document.querySelectorAll('.list')
 
 document.querySelectorAll('.check').forEach ((element) => { 
         element.addEventListener("click", () => {
@@ -18,19 +17,28 @@ document.querySelectorAll('.check').forEach ((element) => {
     }
 )
 
-let list = document.querySelector('.list');
+document.querySelectorAll('.delete').forEach ((element) => { 
+    element.addEventListener("click", () => {
+        element.parentNode.classList.toggle('despair')
+    })
+    console.log(element)
+}
+)
+
+let list=document.querySelector('.list')
 
 function add(){
     if(document.querySelector('#input').value!=''){
         list.innerHTML += `
-            <span>
-            <label for="" class="row lord" style="gap: 2.5rem;">
-            <span class="check"></span>
-            <span class="list row">
-                ${document.querySelector('#input').value}
-            </span>
-            </label>
-            </span>
+        <span>
+        <label for="" class="row lord" style="gap: 2.5rem;">
+        <span class="check"></span>
+        <span class="list row">
+            ${document.querySelector('#input').value}
+        </span>
+        <span class="delete"></span>
+        </label>
+        </span>
         `;
     }
     document.querySelector('#input').value='';
@@ -41,6 +49,13 @@ function add(){
         })
         console.log(element)
     }
+    )
 
-)
+    document.querySelectorAll('.delete').forEach ((element) => { 
+        element.addEventListener("click", () => {
+            element.parentNode.classList.toggle('despair')
+        })
+        console.log(element)
+    }
+    )
 }
